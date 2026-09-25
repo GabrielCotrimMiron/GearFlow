a961c9ddaad9e753f8b411bd26c358d3452d94a5
tato <tatofelicio2006@gmail.com>
2026-09-25 16:41:03 -0300

    Primeiro commit

---
+++ gearflow-system/src/main/java/com/gearflow/gearflow_system/GearflowSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gearflow.gearflow_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearflowSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearflowSystemApplication.class, args);
+	}
+
+}
---
+++ gearflow-system/src/test/java/com/gearflow/gearflow_system/GearflowSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gearflow.gearflow_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearflowSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
